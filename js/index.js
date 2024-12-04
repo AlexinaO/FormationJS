db9458bc7761019404c8deb824332afc9438b20e
Alexina <alexina.orsoni@gmail.com>
2024-12-04 12:00:40 +0100

    Ajout fonction js + écouteur d'évènement pour le chargement du DOM

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 /*Fichier principal de l'application*/
-let jsnotif = document.querySelector('#js-notification');
-jsnotif.style.backgroundColor = 'green';
-// document.querySelector('#js-notification').style.backgroundColor = 'green';
-// document.querySelector('#js-notification').innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>'
-jsnotif.innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>'
+
+function loadingDOM(){
+	let jsnotif = document.querySelector('#js-notification');
+	jsnotif.style.backgroundColor = 'green';
+	// document.querySelector('#js-notification').style.backgroundColor = 'green';
+	// document.querySelector('#js-notification').innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>'
+	jsnotif.innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>'
+}
+
+// loadingDOM();
+
+document.addEventListener('DOMContentLoaded', loadingDOM);
+
+
+
+
 
