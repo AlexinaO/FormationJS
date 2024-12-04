973bbd0ea25a36dcbcb6a8c82089141ddf4a3b6c
Alexina <alexina.orsoni@gmail.com>
2024-12-04 12:37:53 +0100

    Finalement on garde bouton du header avec son évènement

--- js/index.js
+++ js/index.js
@@ -2,22 +2,22 @@
 
 function loadingDOM() {
   document.querySelector("#js-notification").remove();
-  document.querySelector("#header > button").remove();
+  //   document.querySelector("#header > button").remove();
   //   jsnotif.style.backgroundColor = "green";
   //   // document.querySelector('#js-notification').style.backgroundColor = 'green';
   //   // document.querySelector('#js-notification').innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>'
   //   jsnotif.innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>';
-  //   document
-  //     .querySelector("#header > button")
-  //     .addEventListener("click", function (evt) {
-  //       evt.stopPropagation();
-  //       console.log("button header", evt);
-  //       console.log("J'ai cliqué sur le bouton !");
-  //     });
-  document.querySelector("#header").addEventListener("click", function (evt) {
-    console.log("header", evt);
-    console.log("header cliqué !");
-  });
+  document
+    .querySelector("#header > button")
+    .addEventListener("click", function (evt) {
+      evt.stopPropagation();
+      console.log("button header", evt);
+      console.log("J'ai cliqué sur le bouton !");
+    });
+  //   document.querySelector("#header").addEventListener("click", function (evt) {
+  //     console.log("header", evt);
+  //     console.log("header cliqué !");
+  //   });
 }
 
 // loadingDOM();
