583bb148247adc37b962bb8b64892c0dbcb01d7f
Alexina <alexina.orsoni@gmail.com>
2024-12-04 12:11:23 +0100

    Ajout d'un bouton clickable sur le header

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 /*Fichier principal de l'application*/
 
-function loadingDOM(){
-	let jsnotif = document.querySelector('#js-notification');
-	jsnotif.style.backgroundColor = 'green';
-	// document.querySelector('#js-notification').style.backgroundColor = 'green';
-	// document.querySelector('#js-notification').innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>'
-	jsnotif.innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>'
+function loadingDOM() {
+  let jsnotif = document.querySelector("#js-notification");
+  jsnotif.style.backgroundColor = "green";
+  // document.querySelector('#js-notification').style.backgroundColor = 'green';
+  // document.querySelector('#js-notification').innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>'
+  jsnotif.innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>';
+  document
+    .querySelector("#header > button")
+    .addEventListener("click", function () {
+      console.log("J'ai cliqué sur le bouton !");
+    });
 }
 
 // loadingDOM();
 
-document.addEventListener('DOMContentLoaded', loadingDOM);
-
-
-
-
-
+document.addEventListener("DOMContentLoaded", loadingDOM);
