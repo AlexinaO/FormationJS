3420e6a165c5d607253e9c4a0c43a513ba643976
Alexina <alexina.orsoni@gmail.com>
2024-12-04 11:03:43 +0100

    Mise du bandeau en vert et avec JS OK en rouge

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 /*Fichier principal de l'application*/
-alert("Le fichier js est chargé");
+let jsnotif = document.querySelector('#js-notification');
+jsnotif.style.backgroundColor = 'green';
+// document.querySelector('#js-notification').style.backgroundColor = 'green';
+// document.querySelector('#js-notification').innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>'
+jsnotif.innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>'
+
