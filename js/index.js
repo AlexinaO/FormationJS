af670e641fdcb7b4877bea0e705475240cf152a8
Alexina <alexina.orsoni@gmail.com>
2024-12-04 12:35:53 +0100

    Suppression de la bannière et du bouton du header quand le js est chargé

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
 /*Fichier principal de l'application*/
 
 function loadingDOM() {
-  let jsnotif = document.querySelector("#js-notification");
-  jsnotif.style.backgroundColor = "green";
-  // document.querySelector('#js-notification').style.backgroundColor = 'green';
-  // document.querySelector('#js-notification').innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>'
-  jsnotif.innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>';
-  document
-    .querySelector("#header > button")
-    .addEventListener("click", function (evt) {
-      evt.stopPropagation();
-      console.log("button header", evt);
-      console.log("J'ai cliqué sur le bouton !");
-    });
+  document.querySelector("#js-notification").remove();
+  document.querySelector("#header > button").remove();
+  //   jsnotif.style.backgroundColor = "green";
+  //   // document.querySelector('#js-notification').style.backgroundColor = 'green';
+  //   // document.querySelector('#js-notification').innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>'
+  //   jsnotif.innerHTML = 'JS <span style="font-weight:900; color:red;">OK</span>';
+  //   document
+  //     .querySelector("#header > button")
+  //     .addEventListener("click", function (evt) {
+  //       evt.stopPropagation();
+  //       console.log("button header", evt);
+  //       console.log("J'ai cliqué sur le bouton !");
+  //     });
   document.querySelector("#header").addEventListener("click", function (evt) {
     console.log("header", evt);
     console.log("header cliqué !");
