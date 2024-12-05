7cfbe0166ebe945a1682967de70dfd872cb5cc6c
Alexina <alexina.orsoni@gmail.com>
2024-12-05 18:08:34 +0100

    Utilisation de fonctions pour les listener

--- js/editor.js
+++ js/editor.js
@@ -9,6 +9,18 @@ function loadEditor(params) {
   });
 }
 
+function treatInputStringEventChange(evt) {
+  currentMeme[evt.target.name] = evt.target.value;
+}
+
+function treatInputNumberEventChange(evt) {
+  currentMeme[evt.target.name] = parseInt(evt.target.value);
+}
+
+function treatCheckedEventChange(evt) {
+  currentMeme[evt.target.name] = evt.target.checked;
+}
+
 function loadEditorEvents() {
   console.log("route", router.getCurrentRoute());
   document.forms["editor-form"].addEventListener("submit", function (evt) {
@@ -16,41 +28,41 @@ function loadEditorEvents() {
     currentMeme.save();
   });
   document.forms["editor-form"]["text"].addEventListener("input", (evt) => {
-    currentMeme.text = evt.target.value;
+    treatInputStringEventChange(evt);
   });
   document.forms["editor-form"]["imageId"].addEventListener("change", (evt) => {
     currentMeme.imageId = Number(evt.target.value);
   });
 
   document.forms["editor-form"]["x"].addEventListener("change", (evt) => {
-    currentMeme.x = parseInt(evt.target.value);
+    treatInputNumberEventChange(evt);
   });
   document.forms["editor-form"]["y"].addEventListener("change", (evt) => {
-    currentMeme.y = parseInt(evt.target.value);
+    treatInputNumberEventChange(evt);
   });
   document.forms["editor-form"]["fontSize"].addEventListener(
     "change",
     (evt) => {
-      currentMeme.fontSize = parseInt(evt.target.value);
+      treatInputNumberEventChange(evt);
     }
   );
   document.forms["editor-form"]["fontWeight"].addEventListener(
     "change",
     (evt) => {
-      currentMeme.fontWeight = evt.target.value;
+      treatInputStringEventChange(evt);
     }
   );
   document.forms["editor-form"]["underline"].addEventListener(
     "change",
     (evt) => {
-      currentMeme.underline = evt.target.checked;
+      treatCheckedEventChange(evt);
     }
   );
   document.forms["editor-form"]["italic"].addEventListener("change", (evt) => {
-    currentMeme.italic = evt.target.checked;
+    treatCheckedEventChange(evt);
   });
   document.forms["editor-form"]["color"].addEventListener("change", (evt) => {
-    currentMeme.color = evt.target.value;
+    treatInputStringEventChange(evt);
   });
 }
 
